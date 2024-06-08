913c81367a55e92d921ee7fd8fa6791d9cdfcede
Faguim <fagnermuniz12@gmail.com>
2024-06-08 13:54:44 -0300

    refactor: enviroment

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.enableCors();
-  await app.listen(3000 || process.env.PORT, '0.0.0.0');
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
